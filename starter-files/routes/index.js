6ad89a24906929649fa1691a19fdc6b078e815b4
erv <ervinmitra@gmail.com>
2019-06-24 12:52:33 -0700

    modded routes

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
+// Do work here. req has all the info, res has all the methods
+// for sending the data back.
 router.get('/', (req, res) => {
   res.send('Hey! It works!');
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
