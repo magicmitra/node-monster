3a35a6cc938c6e28aee49b3284c353c0303e7bec
erv <ervinmitra@gmail.com>
2019-06-25 16:28:05 -0700

    controller logic for updating business

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -6,18 +6,43 @@ exports.homePage = (req, res) => {
     res.render('index');
 };
 
+// calls the form to render form
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
 
+// actual adding of store into DB
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}. Leave a review, or not.`)
     res.redirect(`/store/${store.slug}`);
 };
 
-exports.getStores = async(req, res) => {
+exports.getStores = async (req, res) => {
     // Query the DB for a list of all stores
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores });
+};
+
+// finds a store and calls the form to render
+exports.editStore = async (req, res) => {
+    // 1. Find store given the ID
+    const store = await Store.findOne({ _id: req.params.id });
+    // 2. Confirm owner of store
+    // TODO
+    // 3. Render out the edit form so user can update the store
+    res.render('editStore', { title: `Edit ${store.name}`, store })
+};
+
+// actual update of a store into DB
+exports.updateStore = async (req, res) => {
+    // find and update store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, // return new store instead of old
+        runValidators: true, // force validators on model to run again
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>
+    <a href="/stores/${store.slug}">View Store</a>`);
+    // redirect into store and confirm success
+    res.redirect(`/stores/${store._id}/edit`);
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -5,12 +5,16 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here. req has all the info, res has all the methods
 // for sending the data back.
+// dont be a fool, wrap your tool. For async functions.
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-// dont be a fool, wrap your tool.
 router.post('/add', catchErrors(storeController.createStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
 
-
+// NOTE: req.params can be accessed anytime a URL contains a 'wildcard'
+// ':value'. Colon, then actual value. Now you fucking know.
+// Dynamic URLS buhzitches
 
 module.exports = router;
