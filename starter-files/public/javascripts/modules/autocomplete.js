b81ec70b586b9cb7f94b57924ac5bb3406a63868
erv <ervinmitra@gmail.com>
2019-06-27 13:36:29 -0700

    missing API key acknowledgement

--- starter-files/public/javascripts/modules/autocomplete.js
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -1,7 +1,9 @@
 function autocomplete(input, latInput, lngInput) {
     if(!input) return; // skip function from running if there
                        // is no input on the page
-    const dropdown = new google.maps.places.Autocomplete(input);
+    // none of this shit works until we get a valid API key
+    // Oh fucking well
+    const dropdown = new google.maps.places.Autocomplete(input); 
 }
 
 export default autocomplete;
