469fc5d4cc1025262ffd0b5e4e20449620a2ded1
erv <ervinmitra@gmail.com>
2019-06-27 13:07:31 -0700

    intergrate google maps

--- starter-files/public/javascripts/modules/autocomplete.js
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -1,5 +1,7 @@
 function autocomplete(input, latInput, lngInput) {
-
+    if(!input) return; // skip function from running if there
+                       // is no input on the page
+    const dropdown = new google.maps.places.Autocomplete(input);
 }
 
 export default autocomplete;
