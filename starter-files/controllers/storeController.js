462059d90f665d6510d072d64b02d94f5c8544fb
erv <ervinmitra@gmail.com>
2019-06-28 14:30:51 -0700

    more resilient slugs

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -85,6 +85,8 @@ exports.updateStore = async (req, res) => {
 };
 
 // getting store by slug
-exports.getStoreBySlug = async (req, res) => {
-    const store = await Store.findOne({ slug: req.params.slug });
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug }); 
+    if(!store) return next();
+    res.render('store', { store, title: store.name });
 };
--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -37,14 +37,19 @@ const storeSchema = new mongoose.Schema({
 
 // pre save hook on 'slug' property
 // code executes before saving 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
     // if name isnt modified, theres no need to run slug
     if(!this.isModified('name')) {
         return next(); // skip and exit function
     }
     this.slug = slug(this.name);
+    // find other stores of same slug
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
     next();
-    // TODO: make more resilient so slugs are unique
-})
+});
 
 module.exports= mongoose.model('Store', storeSchema);
